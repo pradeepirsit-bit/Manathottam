3c5d8502952c7a07ba85f5caf95247cb6d9f3a04
pradeepirsit-bit <pradeepirsit@gmail.com>
2026-06-23 00:38:35 +0530

    Update service worker cache name and optimize fetch

--- sw.js
+++ sw.js
@@ -1,44 +1,31 @@
-// sw.js – Service Worker for LaunchPad
-const CACHE_NAME = 'launchpad-v1';
+const CACHE_NAME = 'moodgarden-v1';
 const urlsToCache = [
   './',
   './index.html',
-  // Add any other assets (CSS, JS, images) your app uses
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
   );
 });
